9bda598910aab2435b8552343d42a3032709af1e
Matvei Bohush <matvei.bohush@gmail.com>
2021-10-11 23:34:13 +0300

    ref

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ffffff',
+        backgroundColor: '#f6d63f',
         alignItems: 'center',
         justifyContent: 'center',
     },
